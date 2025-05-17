d7e34213276e2479a3f82a05f176e4b58654fbb3
Marco Antonio Miranda <marcoa.miranda@usp.br>
2025-05-17 19:39:26 -0300

    Update spotify_data.py

--- utils/spotify_data.py
+++ utils/spotify_data.py
@@ -1,9 +1,20 @@
+# utils/spotify_data.py
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import streamlit as st
 
 def coletar_dados_spotify(max_itens=50):
+    """
+    Coleta dados reais do Spotify com base em lançamentos recentes e trata campos ausentes.
+    
+    Args:
+        max_itens (int): Quantidade máxima de músicas a serem coletadas.
+        
+    Returns:
+        pd.DataFrame: DataFrame com informações das músicas coletadas.
+    """
     if "SPOTIFY_CLIENT_ID" not in st.secrets or "SPOTIFY_CLIENT_SECRET" not in st.secrets:
         st.error("Credenciais do Spotify não configuradas.")
         st.stop()
@@ -21,23 +32,32 @@ def coletar_dados_spotify(max_itens=50):
         while len(resultados) < max_itens:
             limite = min(50, max_itens - len(resultados))
             dados = sp.new_releases(country='BR', limit=limite, offset=offset)
-            albuns = dados['albums']['items']
+            albuns = dados.get('albums', {}).get('items', [])
 
             if not albuns:
                 break
 
             for album in albuns:
-                artista = album['artists'][0]
-                artista_info = sp.artist(artista['id'])
+                artista = album.get('artists', [{}])[0]
+                artista_id = artista.get('id')
 
+                # Pega informações do artista se possível
+                artista_info = {}
+                if artista_id:
+                    try:
+                        artista_info = sp.artist(artista_id)
+                    except:
+                        artista_info = {}
+
+                # Monta os dados do álbum com fallbacks seguros
                 resultados.append({
-                    'conteudo': album['name'],
-                    'artista': artista['name'],
-                    'popularidade': album['popularity'],
+                    'conteudo': album.get('name', 'Nome não disponível'),
+                    'artista': artista.get('name', 'Artista desconhecido'),
+                    'popularidade': album.get('popularity', None),  # Pode ser None
                     'genero': ', '.join(artista_info.get('genres', ['Não informado'])[:3]),
-                    'data_lancamento': album['release_date'],
+                    'data_lancamento': album.get('release_date', 'Data não disponível'),
                     'fonte': 'Spotify',
-                    'link': album['external_urls']['spotify']
+                    'link': album.get('external_urls', {}).get('spotify', '#')
                 })
 
             offset += limite
