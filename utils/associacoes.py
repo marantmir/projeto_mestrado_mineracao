c26f70a9808bbcc4bac41b44e5a401f4347dbd16
Marco Antonio Miranda <marcoa.miranda@usp.br>
2025-05-17 20:01:31 -0300

    Update associacoes.py

--- utils/associacoes.py
+++ utils/associacoes.py
@@ -1,4 +1,11 @@
 # Simulação da aplicação do algoritmo Apriori
 
+from collections import Counter
+
 def gerar_associacoes(df):
-    return [('liberdade', 'sucesso')], ['liberdade', 'sucesso', 'treino']
+    temas = []
+    for texto in df['conteudo']:
+        if isinstance(texto, str):
+            temas.extend([palavra.lower() for palavra in texto.split() if len(palavra) > 3])
+    associacoes = Counter(temas).most_common(20)
+    return associacoes, temas
