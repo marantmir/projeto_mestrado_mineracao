48ad664b084df498a72d00c083e7fc6581218b9d
Marco Antonio Miranda <marcoa.miranda@usp.br>
2025-07-04 08:30:52 -0300

    Update firebase_manager.py

--- data/firebase_manager.py
+++ data/firebase_manager.py
@@ -2,10 +2,13 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import pandas as pd
 import json
+import streamlit as st
+import os
 
 def inicializar_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_key.json")
+        key_dict = json.loads(st.secrets["firebase_key"])
+        cred = credentials.Certificate(key_dict)
         firebase_admin.initialize_app(cred)
 
 def salvar_df_firestore(df, colecao):
